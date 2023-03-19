0b69c16c24602995fb76ce2cfedbcdbf6a93069f
Danilo Rojas Pamplona <danilo.rojas@premexcorp.com>
2023-03-19 17:37:57 -0500

    Initial commit

---
+++ app/src/main/java/com/clase1/sharedpreferences/MainActivity.java
@@ -0,0 +1,14 @@
+package com.clase1.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
